204b6eb5037ef0e746dd08a9ea29690c54177abd
Bruna Luchini <brunaluchini22@gmail.com>
2022-03-11 20:50:25 -0300

    retirando os returns

--- exercicios/exer01.js
+++ exercicios/exer01.js
@@ -80,7 +80,6 @@ const smallerName = (books) => {
       nameBook = book.name;
     }
   });
-
   return nameBook
 }
 
@@ -88,43 +87,38 @@ const smallerName = (books) => {
 
 // EXERCICIO 3
 
-const getNamedBook = (books) => {
-  return books.find(book => book.name.length === 26);
-}
+const getNamedBook = (books) => books.find(book => book.name.length === 26);
+
 
 // retorna o livro que tenha um nome com exatamente 26 caracteres
 
 // EXERCICIO 4
 
-const booksOrderedByReleaseYearDesc =(books) => {
-  return books.sort((a, b) => a.releaseYear > b.releaseYear ? 1 : a.releaseYear === b.releaseYear ? 0 : -1)
-}
+const booksOrderedByReleaseYearDesc = (books) => books.sort((a, b) => a.releaseYear > b.releaseYear ? 1 : a.releaseYear === b.releaseYear ? 0 : -1)
+
 
 // ordena os livros de acordo com o ano de publicação
 
 // EXERCICIO 5
 
-const everyoneWasBornOnSecXX = (books) => {
-  return books.every((book) => book.author.birthYear <= 2000 && book.author.birthYear >= 1901);
-}
+const everyoneWasBornOnSecXX = (books) => books.every((book) => book.author.birthYear <= 2000 && book.author.birthYear >= 1901);
+
 
 // retorna verdadeiro ou falso se todos os autores naceram no século XX
 
 // EXERCÍCIO 6
 
-const someBookWasReleaseOnThe80s = (books) => {
- return books.some((book) => book.releaseYear >= 1980 && book.releaseYear < 1990);
-}
+const someBookWasReleaseOnThe80s = (books) => books.some((book) => book.releaseYear >= 1980 && book.releaseYear < 1990);
+
 
 // retorna verdadeiro ou falso se algum livro foi lançado na década de 80
 
 // EXERCICIOS 7
 
-const authorUnique = (books) => {
-  return books.every(book => !books.some(bookSome => 
-    bookSome.author.birthYear === book.author.birthYear) 
-    && (bookSome.author.name !== book.author.name));
-}
+const authorUnique = (books) => books.every(book => !books.some(bookSome =>
+  bookSome.author.birthYear === book.author.birthYear)
+  && (bookSome.author.name !== book.author.name));
+
 
 // retorna verdadeiro ou falso se tiver algum autor com o mesmo nome
 
@@ -132,62 +126,55 @@ const authorUnique = (books) => {
 
 // ECERCÍCIO 1
 
-const formatedBookNames = (books) => {
-  return books.map(book => `${book.name} - ${book.genre} - ${book.author.name}`)
-} 
+const formatedBookNames = (books) => books.map(book => `${ book.name } - ${ book.genre } - ${ book.author.name }`)
+ 
 
 // formata o nomes do livros, gêneros e autores no template: 
 //"nomeDoLivro - genero - autor"
 
 // EXERCICIO 2
 
-const nameAndAge = (books) => {
-  return books.sort((a, b) => (
+const nameAndAge = (books) => books.sort((a, b) => (
     a.releaseYear - a.author.birthYear) - (b.releaseYear - b.author.birthYear))
-      .map(book => ({ age: book.releaseYear - book.author.birthYear, author: book.author.name }));
-}
+    .map(book => ({ age: book.releaseYear - book.author.birthYear, author: book.author.name }));
+
 
 // ordena de acordo com a idade dos autores, do mais novo ao mais velho, e depois formatando no template:
-// { age: idade, author: nomeDoAutor}
+// { age: idade, author: nomeDoAutor
 
 // EXERCICIO 3
 
-const fantasyOrScienceFiction = (books) => {
-  return books.filter(book => book.genre === 'Fantasia' || book.genre === 'Ficção Científica')
-}
+const fantasyOrScienceFiction = (books) => books.filter(book => book.genre === 'Fantasia' || book.genre === 'Ficção Científica')
+
 
 // filtra os apenas os livros do gênero de fantasia e ficção científica
 
 // EXERCÍCIO 4
 
-const oldBooksOrdered = (books) => {
-  return books.sort((a, b) => a.releaseYear - b.releaseYear).filter(book => 2021 - book.releaseYear > 60);
-}
+const oldBooksOrdered = (books) => books.sort((a, b) => a.releaseYear - b.releaseYear).filter(book => 2021 - book.releaseYear > 60);
+
 
 // ordena os livros com mais de 60 anos, do mais antigo para os mais atuais
 
 // EXERCÍCIO 5
 
-const fantasyOrScienceFictionAuthors = (books) => {
-  return fantasyOrScienceFiction(books
+const fantasyOrScienceFictionAuthors = (books) => fantasyOrScienceFiction(books
     .sort((a, b) => a.author.name > b.author.name ? 1 : a.author.name === b.author.name ? 0 : -1))
     .map(book => book.author.name);
-}
+
 
 // retorna, em ordem alfabética, os autores apenas das obras de ficção científica e fantasia
 
 // EXERCÍCIO 6
 
-const oldBooks = (books) => {
-  return books.filter(book => 2021 - book.releaseYear > 60).map(book => book.name);
-}
+const oldBooks = (books) => books.filter(book => 2021 - book.releaseYear > 60).map(book => book.name);
+
 
 // filtra os livros com mais de 60 anos e retorna seus nomes
 
 // EXERCICIO 7
 
-const authorWith3DotsOnName = (books) => {
-  return books.filter(book => book.author.name.split('').filter(dot => dot === '.').length === 3);
-}
+const authorWith3DotsOnName = (books) => books.filter(book => book.author.name.split('').filter(dot => dot === '.').length === 3);
+
 
 // filtra os nomes dos autores com 3 iniciais neles
