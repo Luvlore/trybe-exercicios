70016bd2eb559cb0e7793bb565d443a08501b762
Bruna Luchini <brunaluchini22@gmail.com>
2022-03-14 17:31:13 -0300

    exercicios 8.4

---
+++ exercicios/exer05.js
@@ -0,0 +1,11 @@
+const names = [
+  'Aanemarie', 'Adervandes', 'Akifusa',
+  'Abegildo', 'Adicellia', 'Aladonata',
+  'Abeladerco', 'Adieidy', 'Alarucha',
+];
+
+const containsA = () => {
+  return names.join('').toLowerCase().split('').reduce((acc, letter) => letter === 'a' ? acc += 1 : acc, 0)
+}
+
+console.log(containsA());
