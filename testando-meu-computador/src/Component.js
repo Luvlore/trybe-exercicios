30c3479ef6874f73b98a8cb3a4c172e3112c6317
Bruna Luchini <brunaluchini22@gmail.com>
2022-04-05 13:27:35 -0300

    exercicios 10.1

---
+++ testando-meu-computador/src/Component.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Element extends React.Component {
+  render() {
+    return <>
+    <h1>Bruna Luchini</h1>
+    <p>Olá, meu nome é Bruna Luchini e eu não sei o que estou fazendo da minha vida.</p>
+    </>
+  }
+}
+
+export default Element;
